753091adeb7caab491fae2ca7c8d1b574fff377d
Elian0 <>
2026-09-13 01:49:39 -0400

    fix: aumentar limite de PWA a 5MB para build en Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -31,6 +31,7 @@ export default defineConfig({
         ]
       },
       workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5 MB
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
         runtimeCaching: [
           {
